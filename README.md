a80670461e20c15dd41c8761884e91e8e3653432
Ualace Henrique Santos Café <ualacehenrique@hotmail.com>
2022-08-11 15:48:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+![ruby_life_logo](assets/logo.png)
+
+[![Ko-Fi](https://img.shields.io/static/v1?message=Buy%20me%20a%20coffee&logo=kofi&labelColor=ff5e5b&color=434B57&logoColor=white&label=%20)](https://ko-fi.com/ualacecafe)
+
+## What's Ruby's Life
+
+Ruby's Life is a recreation of the well-known cellular automaton [_Conway's Game of Life_](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) written in Ruby using the [Ruby2D gem](https://github.com/ruby2d/ruby2d).
+
+## How to Play
+
+```ruby life.rb```
+
+| Control           | Description                                                                 |
+|:------------------|:----------------------------------------------------------------------------|
+| Left Mouse Button | Draws a cell                                                                |
+| P                 | Pauses the game                                                             |
+| R                 | Resets the grid to the previous state (i.e. before the last cell was drawn) |
+| C                 | Clears the screen                                                           |                                                            
+| ESC               | Exit the game                                                               |
+
+## Credits
+
+* John Horton Conway for coming up with the Game of Life;
+* [Ruby2D's](https://github.com/ruby2d/ruby2d) developers for creating this amazing gem.
+* [Daniel Shiffman](https://www.youtube.com/channel/UCvjgXvBlbQiydffZU7m1_aw) for the inspiration.
---
+++ life.rb
@@ -0,0 +1,124 @@
+exit if Object.const_defined?(:Ocra)
+
+require 'ruby2d'
+
+set(title: "Conway's Game of Life", width: 600, height: 400)
+
+WIDTH = Window.width
+HEIGHT = Window.height
+SIZE = 10
+COLS = WIDTH / SIZE
+ROWS = HEIGHT / SIZE
+
+grid = Array.new(ROWS * COLS).map! { |_| 0 }
+previous_grid = Array.new(ROWS * COLS).map! { |_| 0 }
+paused = true
+paused_text = Text.new('PAUSED')
+
+puts '* CONTROLS:'
+puts '  Left Mouse Button - Draws a cell'
+puts '  P - Pauses the game'
+puts '  R - Resets the grid to the previous state (i.e. before the last cell was drawn)'
+puts '  C - Clears the screen'
+puts '  ESC - Exit the game'
+
+def index(x, y)
+  x + y * COLS
+end
+
+def count_neighbors(grid, i)
+  x = i % COLS
+  y = i / COLS
+  sum = 0
+  (-1..1).each do |c|
+    (-1..1).each do |r|
+      next if (x + c).negative? || ((x + c) >= COLS) || (y + r).negative? || ((y + r) >= ROWS)
+
+      col = (x + c) % COLS
+      row = (y + r) % ROWS
+      n = index(col, row)
+      sum += grid[n]
+    end
+  end
+  sum -= grid[i]
+end
+
+on :key_down do |event|
+  case event.key
+  when 'escape'
+    close
+  when 'p'
+    paused = paused ? false : true
+  when 'r'
+    paused = true
+    grid = previous_grid
+  when 'c'
+    grid.map! { |_| 0 }
+  end
+end
+
+on :mouse_down do |event|
+  case event.button
+  when :left
+    x = Window.mouse_x / SIZE
+    y = Window.mouse_y / SIZE
+    i = index(x, y)
+    grid[i] = grid[i].zero? ? 1 : 0
+    previous_grid = grid
+  end
+end
+
+render do
+  COLS.times do |j|
+    ROWS.times do |i|
+      x = j * SIZE
+      y = i * SIZE
+      if grid[index(j, i)] == 1
+        Square.draw(
+          x: x, y: y, size: SIZE, color: [
+            [0.55, 0.84, 0.68, 1],
+            [0.55, 0.84, 0.68, 1],
+            [0.55, 0.84, 0.68, 1],
+            [0.55, 0.84, 0.68, 1]
+          ]
+        )
+      else
+        Square.draw(
+          x: x, y: y, size: SIZE, color: [
+            [0.08, 0.12, 0.1, 1],
+            [0.08, 0.12, 0.1, 1],
+            [0.08, 0.12, 0.1, 1],
+            [0.08, 0.12, 0.1, 1]
+          ]
+        )
+      end
+    end
+  end
+
+  paused_text.draw(x: (WIDTH / 2) - 50, y: (HEIGHT / 2) - 15, rotate: 0, size: 31, color: [0, 0, 0, 1]) if paused
+  paused_text.draw(x: (WIDTH / 2) - 50, y: (HEIGHT / 2) - 15, rotate: 0, size: 30, color: [1, 0.69, 0, 1]) if paused
+end
+
+update do
+  clear
+
+  unless paused
+    next_gen = []
+
+    (COLS * ROWS).times do |i|
+      state = grid[i]
+      n = count_neighbors(grid, i)
+      next_gen[i] = if state.zero? && n == 3
+                      1
+                    elsif state == 1 && (n < 2 || n > 3)
+                      0
+                    else
+                      state
+                    end
+    end
+
+    grid = next_gen
+  end
+end
+
+show
