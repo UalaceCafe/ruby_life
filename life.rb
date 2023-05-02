0b4784b9cf6903ce26e86935fd9ae370cf4513a1
Ualace Henrique Santos Café <ualacehenrique@hotmail.com>
2023-05-01 23:17:24 -0300

    add pattern selection
    Now you can choose between 5 different patterns using the numerical keys (1-5)

--- life.rb
+++ life.rb
@@ -2,7 +2,7 @@ exit if Object.const_defined?(:Ocra)
 
 require 'ruby2d'
 
-set(title: "Conway's Game of Life", width: 600, height: 400)
+set(title: "Ruby's Game of Life", width: 600, height: 400, fps_cap: 60)
 
 WIDTH = Window.width
 HEIGHT = Window.height
@@ -11,19 +11,33 @@ COLS = WIDTH / SIZE
 ROWS = HEIGHT / SIZE
 
 BLACK_COLOR = [0.0, 0.0, 0.0, 1].freeze
-DEAD_COLOR = [0.08, 0.12, 0.1, 1].freeze
+DEAD_COLOR = [0.1, 0.14, 0.14, 1].freeze
 
 grid = Array.new(ROWS * COLS).map! { |_| 0 }
 previous_grid = Array.new(ROWS * COLS).map! { |_| 0 }
+
 paused = true
 paused_text = Text.new('PAUSED')
 
+selected = :cell
+selected_text = Text.new('', size: 12)
+SHAPES = %i[cell block blinker glider gosper_glider_gun].freeze
+
 puts '* CONTROLS:'
 puts '  Left Mouse Button - Draws a cell'
+puts '  1..5 - Select a pattern to be drawn (see below)'
 puts '  P - Pauses the game'
 puts '  R - Returns the grid to the previous state (i.e. before the last cell was drawn)'
 puts '  C - Clears the screen'
 puts '  ESC - Exit the game'
+puts
+puts '* PATTERNS:'
+puts '  1 - Cell'
+puts '  2 - Block'
+puts '  3 - Blinker'
+puts '  4 - Glider'
+puts '  5 - Gosper glider gun'
+puts
 
 def index(x, y)
   x + y * COLS
@@ -57,6 +71,8 @@ on :key_down do |event|
     grid = previous_grid
   when 'c'
     grid.map! { |_| 0 }
+  when '1', '2', '3', '4', '5'
+    selected = SHAPES[event.key.to_i - 1]
   end
 end
 
@@ -66,12 +82,73 @@ on :mouse_down do |event|
     x = Window.mouse_x / SIZE
     y = Window.mouse_y / SIZE
     i = index(x, y)
-    grid[i] = grid[i].zero? ? 1 : 0
+    # I could use a lookup table, but this is probably more efficient (although less readable)
+    case selected
+    when :cell
+      grid[i] = grid[i].zero? ? 1 : 0
+    when :block
+      grid[i] = 1
+      grid[index(x + 1, y)] = 1
+      grid[index(x, y + 1)] = 1
+      grid[index(x + 1, y + 1)] = 1
+    when :blinker
+      grid[i] = 1
+      grid[index(x - 1, y)] = 1
+      grid[index(x + 1, y)] = 1
+    when :glider
+      grid[i] = 1
+      grid[index(x + 1, y + 1)] = 1
+      grid[index(x + 2, y - 1)] = 1
+      grid[index(x + 2, y)] = 1
+      grid[index(x + 2, y + 1)] = 1
+    when :gosper_glider_gun
+      grid[i] = 1
+      grid[index(x + 1, y)] = 1
+      grid[index(x, y + 1)] = 1
+      grid[index(x + 1, y + 1)] = 1
+
+      grid[index(x + 10, y)] = 1
+      grid[index(x + 10, y + 1)] = 1
+      grid[index(x + 10, y + 2)] = 1
+      grid[index(x + 11, y - 1)] = 1
+      grid[index(x + 12, y - 2)] = 1
+      grid[index(x + 13, y - 2)] = 1
+      grid[index(x + 11, y + 3)] = 1
+      grid[index(x + 12, y + 4)] = 1
+      grid[index(x + 13, y + 4)] = 1
+      grid[index(x + 15, y - 1)] = 1
+      grid[index(x + 16, y)] = 1
+      grid[index(x + 14, y + 1)] = 1
+      grid[index(x + 16, y + 1)] = 1
+      grid[index(x + 17, y + 1)] = 1
+      grid[index(x + 16, y + 2)] = 1
+      grid[index(x + 15, y + 3)] = 1
+
+      grid[index(x + 20, y)] = 1
+      grid[index(x + 21, y)] = 1
+      grid[index(x + 20, y - 1)] = 1
+      grid[index(x + 21, y - 1)] = 1
+      grid[index(x + 20, y - 2)] = 1
+      grid[index(x + 21, y - 2)] = 1
+      grid[index(x + 22, y - 3)] = 1
+      grid[index(x + 24, y - 3)] = 1
+      grid[index(x + 24, y - 4)] = 1
+      grid[index(x + 22, y + 1)] = 1
+      grid[index(x + 24, y + 1)] = 1
+      grid[index(x + 24, y + 2)] = 1
+
+      grid[index(x + 34, y - 1)] = 1
+      grid[index(x + 35, y - 1)] = 1
+      grid[index(x + 34, y - 2)] = 1
+      grid[index(x + 35, y - 2)] = 1
+    end
     previous_grid = grid
   end
 end
 
 render do
+  clear
+
   COLS.times do |j|
     ROWS.times do |i|
       x = j * SIZE
@@ -105,11 +182,12 @@ render do
     paused_text.size = 30
     paused_text.draw(x: (WIDTH / 2) - (paused_text.width / 2), y: (HEIGHT / 2) - (paused_text.height / 2), rotate: 0, color: [1, 0.69, 0, 1])
   end
+
+  selected_text.text = "#{selected.to_s.capitalize.gsub('_', ' ')} selected"
+  selected_text.draw(x: 3, y: 2, rotate: 0, color: [1, 1, 1, 0.5])
 end
 
 update do
-  clear
-
   unless paused
     next_gen = []
 
